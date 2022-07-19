8f07fc054e1bad8e93cdc8daeb5dc579506efdd2
Guilherme Magarotti <guimagarotti@hotmail.com>
2022-07-19 10:48:30 -0300

    website-magarotti

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+const App = () => {
+  return (
+    <div>
+      <h1>XUPEX!</h1>
+    </div>
+  );
+}
+
+export default App;
